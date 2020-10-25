e66c558e3e56c5ceab5376380670b1428e42c27b
Mathew <57001761+mathewpius19@users.noreply.github.com>
2020-10-25 18:23:35 +0300

    Added flask background service feature

--- requirements.py
+++ requirements.py
@@ -6,6 +6,7 @@ servername=sys.argv[3]
 os.system(f"echo {password}|sudo -S apt install python3-pip")
 os.system("pip3 install psutil")
 os.system("pip3 install requests")
+os.system(f"echo {password}|sudo -S python3 app.py &")
 os.system(f"python3 report.py {servername} {username} ")
 
 
